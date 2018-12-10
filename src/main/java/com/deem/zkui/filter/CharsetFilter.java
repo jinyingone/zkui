932f5261ad8b38a46574b034ed9eae638988abd8
jy <jinyingshi@sohu-inc.com>
2018-12-10 14:41:05 +0800

    支持中文保存

---
+++ src/main/java/com/deem/zkui/filter/CharsetFilter.java
@@ -0,0 +1,38 @@
+package com.deem.zkui.filter;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
+/**
+ * @author jy
+ * @create 2018-12-10 下午2:34
+ **/
+@WebFilter(filterName = "filtercharset", urlPatterns = "/*")
+public class CharsetFilter implements Filter {
+
+    @Override
+    public void init(FilterConfig fc) throws ServletException {
+        //Do Nothing
+    }
+
+    @Override
+    public void doFilter(ServletRequest req, ServletResponse res, FilterChain fc) throws IOException, ServletException {
+        HttpServletRequest request = (HttpServletRequest) req;
+        HttpServletResponse response = (HttpServletResponse) res;
+
+        request.setCharacterEncoding(StandardCharsets.UTF_8.toString());
+        response.setCharacterEncoding(StandardCharsets.UTF_8.toString());
+
+        fc.doFilter(req, res);
+    }
+
+    @Override
+    public void destroy() {
+        //Do nothing
+    }
+
+}
